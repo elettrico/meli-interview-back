9e6d22ee51863f96af3717bf88a4d8382d0bd809
Fabio Moretti Baratta <fabio.moretti@CENTRAL.ABITAB>
2022-06-28 19:49:45 -0300

    after gitignore modified

--- src/main/java/com/meli/interview/back/subscription_api/user/UserController.java
+++ src/main/java/com/meli/interview/back/subscription_api/user/UserController.java
@@ -2,6 +2,10 @@ package com.meli.interview.back.subscription_api.user;
 
 public class UserController {
 
+    /**
+     * test 
+     * @return
+     */
     public String createUser() {
         return "";
     }
